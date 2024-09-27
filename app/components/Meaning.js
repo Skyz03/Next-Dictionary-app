c26e953ce01a2c574d71e37c8230155d236618f2
Skyz03 <aakibdiscoo@gmail.com>
2024-09-27 10:34:07 +0545

    fix: pro ready

--- app/components/Meaning.js
+++ app/components/Meaning.js
@@ -2,35 +2,30 @@ export default function Meaning({ meanings }) {
   return (
     <div>
       {meanings.map((meaning, meaningIndex) => (
-        <div key={meaningIndex}>
-          {/* Display partOfSpeech and "Meaning" only once per meaning */}
+        <section key={meaningIndex} className="mb-6">
+          {/* Part of speech and divider */}
           <div className="flex flex-row justify-between items-center mb-2">
             <p className="font-bold lowercase my-4">{meaning.partOfSpeech}</p>
             <hr className="w-[85%]" />
           </div>
 
           <p className="font mb-4 text-gray-500">Meaning</p>
-          <ul className="list-disc list-inside ml-5 text-purple-600">
-            {/* Now map over definitions */}
+
+          <ul className="ml-5">
             {meaning.definitions.map((definition, defIndex) => (
-              <li
-                key={defIndex}
-                className="mb-2 text-white"
-                style={{ textIndent: "-1.5em", paddingLeft: "1em" }}
-              >
+              <li key={defIndex} className="mb-2 text-white relative pl-6">
+                <span className="text-purple-600 absolute -left-1">•</span>{" "}
+                {/* Custom purple bullet */}
                 {definition.definition}
                 {definition.example && (
-                  <blockquote
-                    className="italic text-gray-400 mt-2"
-                    style={{ textIndent: "-1.5em", paddingLeft: "1em" }}
-                  >
+                  <blockquote className="italic text-gray-400 mt-2">
                     “{definition.example}“
                   </blockquote>
                 )}
               </li>
             ))}
           </ul>
-        </div>
+        </section>
       ))}
     </div>
   );
